74524f21fc7dfe763fb523f6043c1e733b3e2c23
unknown <yashslg004@gmail.com>
2025-02-10 19:53:08 +0530

    Added Error Element

--- frontend/src/ui/Error.jsx
+++ frontend/src/ui/Error.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link, useNavigate, useRouteError } from 'react-router-dom';
 
 function Error() {
+  const error = useRouteError();
+  const navigate = useNavigate();
   return (
-    <div className="bg-indie-500 text-indie-200 flex items-center justify-center h-screen text-5xl">
-    Something Went Wrong!
+    <div className="bg-indie-500 text-indie-200 flex flex-col gap-4 items-center justify-center h-screen text-5xl">
+    <h1>Something Went Wrong!</h1>
+    <p>{error.data || error.message}</p>
+    <Link onClick={()=>(navigate(-1))} className="cursor-pointer"> ⬅ </Link>
     </div>
   )
 }
