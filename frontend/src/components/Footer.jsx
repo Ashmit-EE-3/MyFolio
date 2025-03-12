841b95b3607353dc14c4abfb4345eb0d3e0d3cc0
unknown <yashslg004@gmail.com>
2025-03-13 00:46:02 +0530

    added favicon

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import logo from "../../public/logo-myfolio.png"
 function Footer() {
   return (
     <div className='font-poppins flex justify-center gap-2 p-8 bg-indie-700 items-center text-sm md:text-lg text-indie-100'>
-        <img src={logo} className="md:h-10 md:w-10 h-6 w-6"/><span className="mt-3">MyFolio.tech © {year}</span> 
+        <img src={logo} className="md:h-10 md:w-10 h-7 w-7"/><span className="md:mt-3 mt-2">MyFolio.tech © {year}</span> 
     </div>
   )
 }
